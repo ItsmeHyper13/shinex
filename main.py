68d5815f49566425bacd983cd5a1be9db90b7c0b
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-07-30 19:30:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# Powered by @HYPER_AD13 | @ShiningOff
+# Dear Pero ppls Plish Don't remove this line from here🌚
+# created by ItsmeHyper13
+
+from pyrogram import Client
+import time
+from pyrogram import idle
+from config import (API_ID, API_HASH, BOT_TOKEN)
+
+sree = Client(
+    ":memory:",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="shinex")
+)
+
+print("Try To Logging.....🌚")
+print("Bot Logging Successfully✅")
+
+
+sree.start()
+idle()
