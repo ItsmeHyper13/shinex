65b92da7343489c75a70a4cd4c2d1f4443070f33
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-07-31 13:51:06 +0530

    Update strs.py

--- strings/strs.py
+++ strings/strs.py
@@ -6,3 +6,4 @@ start_03 = "<strong>Hehe, I am preparing for a good start message for youh 🥰�
 # PING STRINGS
 ping_01 = "<strong><i>Pinging🎑🎊...</i></strong>"
 ping_02 = "<strong><i>Who the hell [You](tg://user?id={}) are??\nYou are not my master huh!!😏😏</i></strong>"
+ping_03 = "<strong><i>Ping Pong 🎇..\n\n<u>Yeah {} is alive!!</u></i>\n❅Server Ping: <code>{} ms</code>\n❅Bot Ping</b>: <code>{}</code>\n❅Uptime: <code>{}</code>\n❅Python Version: <code>{}</code>\n❅Pyrogram Version: <code>{}</code></strong>"
