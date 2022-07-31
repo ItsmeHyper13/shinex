2e5bfc61f876ea308b7efbdd1247420f568eca26
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-07-31 12:06:24 +0530

    Update start.py

--- shinex/start.py
+++ shinex/start.py
@@ -2,9 +2,10 @@
 # Dear Pero ppls Plish Don't remove this line from here🌚
 # created by ItsmeHyper13
 import time, datetime
-from asyncio import sleep as rst
+from asyncio import sleep as rest
 from pyrogram import Client as sree
 from pyrogram import filters
+from vars import OWNER_ID
 from strings import handlers as hndl, strs as txt
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
@@ -13,8 +14,20 @@ START = hndl.START_CMD
 @sree.on_message(filters.command(START))
 async def start(sree, m: Message):
     user = m.from_user
-    await m.reply(
+    me = await sree.get_user("me")
+    owner = await sree.get_user(OWNER_ID)
+    e = await m.reply(
         (txt.start_01).format(
             user.mention
         )
     )
+    await rest(2)
+    await e.edit_text(
+        (txt.start_02).format(
+            me.first_name,
+            owner.mention
+        )
+    )
+    await rest(2)
+    await e.edit_text(txt.start_03)        
+    
