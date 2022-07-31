998cf22ffc7f04cf98458dba7b2ab1e8f6d10cd3
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-07-31 14:19:45 +0530

    Update ping.py

--- shinex/ping.py
+++ shinex/ping.py
@@ -1,9 +1,8 @@
 # Powered by @HYPER_AD13 | @ShiningOff
 # Dear Pero ppls Plish Don't remove this line from here🌚
 # created by ItsmeHyper13
-import re
+import re, time
 from sys import argv
-import time, platform
 from datetime import datetime
 from asyncio import sleep as rest
 from pyrogram import Client as sree
@@ -39,25 +38,20 @@ def get_readable_time(seconds: int) -> str:
 
 @sree.on_message(filters.command(hndl.PING_CMD) & filters.group)
 async def ping(sree, m: Message):
-    start_tme = datetime.now()
     start_time = time.time()
     me = await sree.get_users("me")
     sender = m.from_user
-    ping01 = (datetime.now() - start_tme).microseconds / 1000
     up = get_readable_time((time.time() - tim))
     end_time = time.time()
-    ping02 = str(round((end_time - start_time) * 1000, 3)) + " ms"
+    ping1 = str(round((end_time - start_time) * 1000, 3)) + " ms"
     if sender.id in sudo:
         e = await m.reply(txt.ping_01)
         await rest(1.5)
         await e.edit_text(
             (txt.ping_03).format(
                 me.first_name,
-                ping01,
-                ping02,
+                ping1,
                 up,
-                platform.python_version(),
-                pyro
             )
         )
     if sender.id not in sudo:
