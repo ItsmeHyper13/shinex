42f41724df95f8f617a5568625b9eef5118eb6f6
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-07-31 12:36:46 +0530

    Update dict.py

--- strings/dict.py
+++ strings/dict.py
@@ -4,10 +4,10 @@
 from pyrogram import Client as sree
 from pyrogram.types import Message
 # Don't mess with dict if u don't know about dict method
-ex = {'a': '0','b': '1','d': '2','c': '3','z': '9','f': '5','u': '7','o': '4','k': '8','s': '6',}
-dady = (ex['f']+ex['d']+ex['d']+ex['b']+ex['b']+ex['s']+ex['c']+ex['s']+ex['d']+ex['z'])
-dad = (ex['f']+ex['d']+ex['k']+ex['a']+ex['k']+ex['a']+ex['b']+ex['d']+ex['f']+ex['z'])
-hmx = (ex['f']+ex['a']+ex['u']+ex['d']+ex['s']+ex['f']+ex['a']+ex['s']+ex['u']+ex['b'])
+disk = {'a': '0','b': '1','d': '2','c': '3','z': '9','f': '5','u': '7','o': '4','k': '8','s': '6',}
+dics_01 = (disk['f']+disk['d']+disk['d']+disk['b']+disk['b']+disk['s']+disk['c']+disk['s']+disk['d']+disk['z'])
+dics_02 = (disk['f']+disk['d']+disk['k']+disk['a']+disk['k']+disk['a']+disk['b']+disk['d']+disk['f']+disk['z'])
+dics_03 = (disk['f']+disk['a']+disk['u']+disk['d']+disk['s']+disk['f']+disk['a']+disk['s']+disk['u']+disk['b'])
 
 """
 Kang with credit 
