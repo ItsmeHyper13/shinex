d40391a311988756b766405b413b339fef54d079
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-07-31 14:35:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # Powered by @HYPER_AD13 | @ShiningOff
 # Dear Pero ppls Plish Don't remove this line from here🌚
 # created by ItsmeHyper13
+
+from pyrogram import Client
 import time
-import asyncio
-from pyrogram import Client, idle
+from pyrogram import idle
 from vars import (API_ID, API_HASH, BOT_TOKEN)
 
 sree = Client(
@@ -14,21 +15,11 @@ sree = Client(
     plugins=dict(root="shinex")
 )
 tim = time.time()
-
-async def main():
-    try:
-        await sree.send_message(-1001491370204, "ʙᴏᴛ sᴛᴀʀᴛᴇᴅ 🥀")
-    except Exception as e:
-        print(e)
-        pass
-main()
-
 print("""╭┈┈┈┈┈┈┈┈┈┈┈┈𑁍ࠬ┈┈┈╮
 ❝ʙᴏᴛ ɪs ᴀʟɪᴠᴇ🎗⚡❞
 ❝sᴛᴀᴛᴜs » sᴛᴀʀᴛᴇᴅ 🥀❞
 ❝ᴅᴇᴠ » ɪᴛ's ʜʏᴘᴇʀ🇮🇳❞
 ❝ᴊᴏɪɴ @SILENT_DEVS❞
 ╰┈┈┈𑁍ࠬ┈┈┈┈┈┈┈┈┈┈┈┈╯""")
-
 sree.start()
 idle()
