39f16a62f7a98552c1ddb61301ff0f70a460b705
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-07-31 12:41:57 +0530

    Update strs.py

--- strings/strs.py
+++ strings/strs.py
@@ -4,4 +4,4 @@ start_02 = "<strong>Welcome to {}'s pm!\nI am assistant robot of {} !</strong>"
 start_03 = "<strong>Hehe, I am preparing for a good start message for youh 🥰🐋</strong>"
 
 # PING STRINGS
-ping_01 = "<strong><i>Pinging🎑🎊...</i></strong>
+ping_01 = "<strong><i>Pinging🎑🎊...</i></strong>"
