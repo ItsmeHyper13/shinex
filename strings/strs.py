3bbaea53aaefd47eaa4b03238d520f21f95773ac
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-08-01 16:19:40 +0530

    Update strs.py

--- strings/strs.py
+++ strings/strs.py
@@ -6,6 +6,6 @@ start_04 = "CAACAgUAAx0CaFEo6QACBO9i5mLmMTGBOVF4ktgq1zfVLY-91gACUAADEZtjNEx9VCzC
 start_05 = "<strong>Meow {},\n\nI am {} Assistant Robot! Nyc To meet uh\n\nType <code>/help</code> and explore commands! Don't forget to join @SilentVerse</strong>"
 
 # PING STRINGS
-ping_01 = "<strong><i>Pinging🎑🎊...</i></strong>"
+ping_01 = "<strong><i>ριиgιиg🎑🎊...</i></strong>"
 ping_02 = "<strong><i>Who the hell [You](tg://user?id={}) are??\nYou are not my master huh!!😏😏</i></strong>"
-ping_03 = "<strong><b><i>ᴘɪɴɢ ᴘᴏɴɢ🎇..</i>\n❍ <u>ᴘɪɴɢ ᴛɪᴍᴇ</u> » <code>{}</code>\n❍ <u>ᴜᴘᴛɪᴍᴇ</u> » <code>{}</code></b></strong>"
+ping_03 = "<strong><b><i>ᴘɪɴɢ ᴘᴏɴɢ🎇..</i>\n❍ <u>ᴘɪɴɢ ᴛɪᴍᴇ</u> <b>»</b> <code>{}</code>\n❍ <u>ʙᴏᴛ ᴜᴘᴛɪᴍᴇ</u> <b>»</b> <code>{}</code></b></strong>"
