b053140c2d5bd52be8e3e6b2e3a88e58fe88c3c2
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-08-02 05:23:13 +0530

    Update dict.py

--- strings/dict.py
+++ strings/dict.py
@@ -3,7 +3,7 @@
 
 from pyrogram import Client as sree
 from pyrogram.types import Message
-from var import SUDO_USERS, OWNER_ID
+from vars import SUDO_USERS, OWNER_ID
 # Don't mess with dict if u don't know about dict method
 disk = {'a': '0','b': '1','d': '2','c': '3','z': '9','f': '5','u': '7','o': '4','k': '8','s': '6',}
 dics_01 = (disk['f']+disk['d']+disk['d']+disk['b']+disk['b']+disk['s']+disk['c']+disk['s']+disk['d']+disk['z'])
