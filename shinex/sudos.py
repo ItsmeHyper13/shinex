94099aef7c5387d9f03b94ae61fcfd4e8e4d1aa8
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-08-02 04:37:49 +0530

    Create sudos.py

---
+++ shinex/sudos.py
@@ -0,0 +1,13 @@
+# Powered by @HYPER_AD13 | @ShiningOff
+# Dear Pero ppls Plish Don't remove this line from here🌚
+# created by ItsmeHyper13
+from pyrogram import Client as sree, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from strings import strs as txt, handlers as hndl
+from vars import SUDO_USERS as sudo
+
+@sree.on_message(filters.command(hndl.SUDO_CMD, prefixes=list("./")))
+async def sudos(sree, m: Message):
+    await message.reply(
+        (txt.sudos_01).format(sudo)
+    )            
