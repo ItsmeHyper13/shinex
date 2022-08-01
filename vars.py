5fd72863cef4c299f0b5226a47f66c21b81d71a1
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-08-02 04:58:35 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -10,7 +10,7 @@ API_ID = int(getenv("API_ID", "4110592"))
 API_HASH = getenv("API_HASH", "aa7c849566922168031b95212860ede0")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 OWNER_ID = int(getenv("OWNER_ID", '5072650671'))
-SUDOS = list(map(int, getenv("SUDO_USERS", "5072650671 5280801259 5221163629").split()))
+SUDOS = list(map(int, getenv("SUDO_USERS", "5280801259 5221163629").split()))
 SUDO_USERS = []
 for x in SUDO_USERS:
     SUDO_USERS.append(OWNER_ID)
