cc62c65c2dc640ab4a22a38f3014e2193f46bd7d
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-08-01 20:18:22 +0530

    Update ben.py

--- shinex/ben.py
+++ shinex/ben.py
@@ -4,12 +4,3 @@
 from pyrogram import Client as sree, filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
-@sree.on_message(filters.command('ban', prefixes=['.','/']))
-async def ban(sree, m: Message):
-  await sree.ban_chat_member(m.chat.id, m.reply_to_message.from_user.id)
-  await m.reply_text(f"`Banned Successfully `\n{m.reply_to_message.from_user.mention} Unkil GoAway 😒\n")
-
-@sree.on_message(filters.command('unban', prefixes=[".",'/']))
-async def unban(devu, message: Message):
-  await sree.unban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
-  await message.reply_text(f"`UnBanned Successfully `\n{message.reply_to_message.from_user.mention} Can Join Again 👀\n")
