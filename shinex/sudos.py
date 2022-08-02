8df742d364a99cc0993cbe49376f7ac917063803
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-08-02 09:34:20 +0530

    Update sudos.py

--- shinex/sudos.py
+++ shinex/sudos.py
@@ -4,11 +4,11 @@
 from pyrogram import Client as sree, filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from strings import strs as txt, handlers as hndl
-from vars import SUDO_USERS as sudo
+from vars import SUDO_USERS as sudo, OWNER_ID as owner
 
 @sree.on_message(filters.command(hndl.SUDO_CMD, prefixes=list("./")))
 async def sudos(sree, m: Message):
     if m.from_user.id in sudo:
         await m.reply(
-            (txt.sudos_01).format(sudo)
+            (txt.sudos_01).format(sudo.remove(owner)))
         )            
