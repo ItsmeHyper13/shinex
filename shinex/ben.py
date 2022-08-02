18b924e4b49b001c1c03e25602386384f52008e0
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-08-02 20:53:21 +0530

    Update ben.py

--- shinex/ben.py
+++ shinex/ben.py
@@ -2,5 +2,16 @@
 # Dear Pero ppls Plish Don't remove this line from here🌚
 # created by ItsmeHyper13
 from pyrogram import Client as sree, filters
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from vars import SUDO_USERS as sudo, OWNER_ID as owner
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup     
+from strings import handlers as hndl, strs as txt
 
+@sree.on_message(filters.command(hndl.BAN_CMD, prefixes=list("./")) & filters.group)
+async def banthisgay(sree, m: Message):
+    sender = m.from_user
+    if sender.id in sudo:
+        await app.ban_chat_member(m.chat.id, m.reply_to_message.from_user.id)
+        await app.send_message(
+            m.chat.id,
+            (txt.ban_01).format(
+                
