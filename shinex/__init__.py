710efe41d56e319eae10afcab4e30a8c5d53a268
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-08-24 07:35:13 +0530

    Update __init__.py

--- shinex/__init__.py
+++ shinex/__init__.py
@@ -4,7 +4,7 @@ import logging
 from importlib import import_module
 from os import environ, getenv, listdir, path
 from dotenv import load_dotenv
-from pymongo import MongoClient, errors
+
 from telethon import TelegramClient
 import vars
 from pyrogram import Client
