489dd8105c631e16ce679817116444eb532f076d
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-08-24 07:10:14 +0530

    Create __main__.py

---
+++ shinex/__main__.py
@@ -0,0 +1,19 @@
+
+
+
+from shinex import sree, BOT_TOKEN as TOKEN 
+from shinex.modules import __load_modules
+
+
+sree.start(bot_token=TOKEN)
+    
+        
+__load_modules()
+
+
+sree.run_until_disconnected()     
+    
+
+
+
+
